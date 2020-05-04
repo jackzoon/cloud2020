a5341492b6b27d7b98d610d995b83f0ead9f784d
Halen <911452597@qq.com>
2020-05-04 11:57:17 +0800

    Eureka集群搭建

---
+++ cloud-eureka-server7002/src/main/java/com/halen/springcloud/EurekaMain7002.java
@@ -0,0 +1,16 @@
+package com.halen.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created By Halen 2020/5/4 11:02
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}
