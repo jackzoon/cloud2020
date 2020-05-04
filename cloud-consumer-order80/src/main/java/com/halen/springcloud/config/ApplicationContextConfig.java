6a35330e5ec42be39a3e6a4762eb4c781f8e95c8
Halen <911452597@qq.com>
2020-05-04 15:19:57 +0800

    服服务发现

--- cloud-consumer-order80/src/main/java/com/halen/springcloud/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/halen/springcloud/config/ApplicationContextConfig.java
@@ -1,5 +1,6 @@
 package com.halen.springcloud.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class ApplicationContextConfig {
     @Bean
+    @LoadBalanced //使用这个注解赋予RestTemplate负载均衡的能力
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
---
+++ cloud-provider-payment8002/src/main/java/com/halen/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,29 @@
+package com.halen.springcloud.service.impl;
+
+
+import com.halen.springcloud.entities.Payment;
+import com.halen.springcloud.service.PaymentService;
+import org.springframework.stereotype.Service;
+import com.halen.springcloud.dao.PaymentDao;
+
+import javax.annotation.Resource;
+
+/**
+ * Created By Halen 2020/5/3 10:34
+ */
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+    @Resource
+    private PaymentDao paymentDao;
+
+    @Override
+    public int create(Payment payment) {
+        return paymentDao.create(payment);
+    }
+
+    @Override
+    public Payment getPaymentById(Long id) {
+        return paymentDao.getPaymentById(id);
+    }
+}
