5e54e0a20dcc1478d59e8d629b11390ed1d1ee39
Halen <911452597@qq.com>
2020-05-05 15:57:08 +0800

    Zookeeper作为注册中心

---
+++ cloud-consumerzk-order80/src/main/java/com/halen/springcloud/OrderZKMain80.java
@@ -0,0 +1,16 @@
+package com.halen.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Created By Halen 2020/5/5 15:42
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderZKMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderZKMain80.class, args);
+    }
+}
---
+++ cloud-provider-payment8004/src/main/java/com/halen/springcloud/PaymentMain8004.java
@@ -0,0 +1,16 @@
+package com.halen.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Created By Halen 2020/5/5 13:47
+ */
+@SpringBootApplication
+@EnableDiscoveryClient // 该注解用于向使用consul或者Zookeeper作为注册中心时注册服务
+public class PaymentMain8004 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8004.class, args);
+    }
+}
